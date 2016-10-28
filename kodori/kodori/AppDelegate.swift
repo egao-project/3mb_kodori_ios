ea9a24aeab3e7c7169e36477d3dce4e5b6a08083
ushikit <ushikit@egao-inc.co.jp>
2016-10-28 21:00:09 +0900

    no message

--- kodori/kodori/AppDelegate.swift
+++ kodori/kodori/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // １秒待ってから画面遷移
+        sleep(1)
         return true
     }
 
--- kodori/kodori/ViewController.swift
+++ kodori/kodori/ViewController.swift
@@ -1,25 +1,73 @@
-//
-//  ViewController.swift
-//  kodori
-//
-//  Created by 牛木 紀幸 on 2016/10/03.
-//  Copyright © 2016年 牛木 紀幸. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    // カメラの映像をここに表示
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSesssion: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    // ボタンを押した時呼ばれる
+    @IBAction func takeIt(_ sender: AnyObject) {
+        // フラッシュとかカメラの細かな設定
+        let settingsForMonitoring = AVCapturePhotoSettings()
+        settingsForMonitoring.flashMode = .auto
+        settingsForMonitoring.isAutoStillImageStabilizationEnabled = true
+        settingsForMonitoring.isHighResolutionPhotoEnabled = false
+        // シャッターを切る
+        stillImageOutput?.capturePhoto(with: settingsForMonitoring, delegate: self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        captureSesssion = AVCaptureSession()
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        captureSesssion.sessionPreset = AVCaptureSessionPreset1920x1080 // 解像度の設定
+        
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            // 入力
+            if (captureSesssion.canAddInput(input)) {
+                captureSesssion.addInput(input)
+                
+                // 出力
+                if (captureSesssion.canAddOutput(stillImageOutput)) {
+                    captureSesssion.addOutput(stillImageOutput)
+                    captureSesssion.startRunning() // カメラ起動
+                    
+                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSesssion)
+                    previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect // アスペクトフィット
+                    previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait // カメラの向き
+                    
+                    cameraView.layer.addSublayer(previewLayer!)
+                    
+                    // ビューのサイズの調整
+                    previewLayer?.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                    previewLayer?.bounds = cameraView.frame
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
     }
-
-
+    
+    // デリゲート。カメラで撮影が完了した後呼ばれる。JPEG形式でフォトライブラリに保存。
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let photoSampleBuffer = photoSampleBuffer {
+            // JPEG形式で画像データを取得
+            let photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            let image = UIImage(data: photoData!)
+            
+            // フォトライブラリに保存
+            UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+        }
+    }
 }
-
